31352e3c0e0b4dfa1cbf8d22e4dc2deb66d0c6e2
Matt Lindley <rmattlindley@gmail.com>
2016-11-05 15:38:12 -0700

    added bootstrap and a basic bootstrap navbar

---
+++ src/components/nav.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { NavBar, Nav, NavItem } from 'react-bootstrap';
+
+const Navigation = React.createClass ({
+  render: function () {
+    return (
+      <nav className="navbar navbar-inverse navbar-static-top">
+        <div className="container">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Project name</a>
+          </div>
+          <ul className="nav navbar-nav navbar-right">
+            <li><a href="#">Add Item</a></li>
+          </ul>
+          <form className="navbar-form navbar-right">
+            <input type="text" className="form-control" placeholder="Search..." />
+          </form>
+        </div>
+      </nav>
+    );
+  }
+});
+
+module.exports = Navigation;
