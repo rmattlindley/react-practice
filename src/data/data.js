6d47b89d1465b2ab856d71b945457ef894daa644
Matt Lindley <rmattlindley@gmail.com>
2016-11-05 22:32:47 -0700

    added some bs fake data

---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+/*
+  data source:
+  https://www.yahoo.com/news/atticus-tops-baby-names-2015-124073377716.html
+*/
+
+export default [
+  {
+    id: 0,
+    name: 'Object 0',
+    details: 'Object details go here'
+  }, {
+    id: 1,
+    name: 'Object 1',
+    details: 'Object details go here'
+  }, {
+    id: 2,
+    name: 'Object 2',
+    details: 'Object details go here'
+  }, {
+    id: 3,
+    name: 'Object 3',
+    details: 'Object details go here'
+  }, {
+    id: 4,
+    name: 'Object 4',
+    details: 'Object details go here'
+  }, {
+    id: 5,
+    name: 'Object 5',
+    details: 'Object details go here'
+  }, {
+    id: 6,
+    name: 'Object 6',
+    details: 'Object details go here'
+  }, {
+    id: 7,
+    name: 'Object 7',
+    details: 'Object details go here'
+  }, {
+    id: 8,
+    name: 'Object 8',
+    details: 'Object details go here'
+  }, {
+    id: 9,
+    name: 'Object 9',
+    details: 'Object details go here'
+  }, {
+    id: 10,
+    name: 'Object 10',
+    details: 'Object details go here'
+  }
+]
